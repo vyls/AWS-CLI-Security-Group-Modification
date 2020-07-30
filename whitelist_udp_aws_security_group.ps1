406f3482ccba4f94840aa13c07cf145651cb6943
vyls <52683521+vyls@users.noreply.github.com>
2020-07-30 12:47:50 +0800

    Create whitelist_udp_aws_security_group.ps1

---
+++ whitelist_udp_aws_security_group.ps1
@@ -0,0 +1,6 @@
+#Created by V. Y. Lindung S
+$whitelist = Get-Content ((Read-Host 'Enter Whitelist File'))
+foreach ($_ in $whitelist){
+$sgparam = $_.split()
+aws ec2 authorize-security-group-ingress --group-id $sgparam[0] --protocol udp --port $sgparam[1] --cidr $sgparam[2]
+}
