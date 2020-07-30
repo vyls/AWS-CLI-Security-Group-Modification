81cdf9c48fce95981f26aa853f4e8a9894d610cf
vyls <52683521+vyls@users.noreply.github.com>
2020-07-30 11:44:27 +0800

    Add files via upload

---
+++ whitelist_aws_security_group.ps1
@@ -0,0 +1,5 @@
+$whitelist = Get-Content ((Read-Host 'Enter Whitelist File'))
+foreach ($_ in $whitelist){
+$sgparam = $_.split()
+aws ec2 authorize-security-group-ingress --group-id $sgparam[0] --protocol tcp --port $sgparam[1] --cidr $sgparam[2]
+}
---
+++ whitelist_aws_security_group.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#input="/var/www/html/whitelist_sg.txt" #change the directory of the list of whitelist IP
+input=$1
+while IFS= read -r line
+do
+aws_security_group=$(echo $line | awk '{print $1}') #will get Security Group ID information
+aws_whitelist_port=$(echo $line | awk '{print $2}') #will get TCP/UDP port information
+aws_whitelist_prefix=$(echo $line | awk '{print $3}') #will get Prefix/Subnet information
+aws ec2 authorize-security-group-ingress --group-id $aws_security_group --protocol tcp --port $aws_whitelist_port --cidr $aws_whitelist_prefix
+done < "$input" #end command
